1f77ccea97e0d764555058f9716a52136290f67b
NicolleMagpale <nicolle_magpale@dlsu.edu.ph>
2017-03-30 04:20:36 +0800

    +AdGuestContactAdapter +^^ EU

---
+++ app/src/main/java/com/example/checkmeet/adapter/AddGuestContactAdapter.java
@@ -0,0 +1,87 @@
+package com.example.checkmeet.adapter;
+
+import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.checkmeet.R;
+import com.example.checkmeet.model.Contact;
+
+import java.util.List;
+
+/**
+ * Created by ASUS-PC on 30/03/2017.
+ */
+
+public class AddGuestContactAdapter extends RecyclerView.Adapter<AddGuestContactAdapter.ContactHolder>
+{
+    private List<Contact> contactList;
+    private LayoutInflater inflater;
+
+    private ContactItemClickCallback contactItemClickCallback;
+
+    public AddGuestContactAdapter(List<Contact> contactList, Context context) {
+        this.contactList = contactList;
+        this.inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public AddGuestContactAdapter.ContactHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = inflater.inflate(R.layout.select_contact_item, parent, false);
+        return new AddGuestContactAdapter.ContactHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(AddGuestContactAdapter.ContactHolder holder, int position) {
+        holder.tvName.setText(contactList.get(position).getName());
+        holder.tvNumber.setText(contactList.get(position).getNumber());
+        holder.tvIcon.setText(contactList.get(position).getName().charAt(0) + "");
+        holder.iconDrawable.setColor(contactList.get(position).getColor());
+    }
+
+    @Override
+    public int getItemCount() {
+        return contactList.size();
+    }
+
+    public void setContactItemClickCallback(
+            final ContactItemClickCallback contactItemClickCallback) {
+        this.contactItemClickCallback = contactItemClickCallback;
+    }
+
+    public void setItems(List<Contact> contactList) {
+        this.contactList = contactList;
+    }
+
+    class ContactHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        TextView tvName, tvNumber, tvIcon;
+        GradientDrawable iconDrawable;
+        View container;
+
+        ContactHolder(View itemView) {
+            super(itemView);
+
+            tvName = (TextView) itemView.findViewById(R.id.tv_name);
+            tvNumber = (TextView) itemView.findViewById(R.id.tv_number);
+            tvIcon = (TextView) itemView.findViewById(R.id.tv_icon);
+            container = itemView.findViewById(R.id.contact_container);
+            iconDrawable = (GradientDrawable) tvIcon.getBackground();
+            container.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if(v.getId() == container.getId()) {
+                Log.e("adapter", "clicked item");
+                contactItemClickCallback.onItemClick(getAdapterPosition());
+            }
+        }
+    }
+}
+
